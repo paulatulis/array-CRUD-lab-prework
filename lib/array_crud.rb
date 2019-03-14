e5227c998bcdf0d45db21280736ed966c8186b11
Paula Tulis <tulis.paula@gmail.com>
2019-03-14 18:57:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,25 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  snacks.unshift("twizzlers")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  snacks.shift
+  array.shift
 end
 
 def remove_element_from_start_of_array(array)
-  snacks.pop
+  array.pop
 end
 
 def retrieve_element_from_index(array, index_number)
-  snacks[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  snacks[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  snacks[-1]
+  array[-1]
 end
